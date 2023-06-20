d1bd925d7b9f230d863432ec683a6e13f9752cfd
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2023-06-20 17:54:44 +0100

    almost finished

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? 'YOUR_PRODUCTION_API_URL' : 'http://127.0.0.1:5000';
+const apiUrl = process.env.NODE_ENV === 'production' ? 'https://8dfc-13-42-13-51.ngrok-free.app' : 'http://127.0.0.1:5000';
 
 export default apiUrl;
