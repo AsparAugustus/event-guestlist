8b9d9204223e12601912d9dc06a815d33e200343
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2023-06-21 18:34:03 +0100

    ngrok site change

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-const apiUrl = process.env.NODE_ENV === 'production' ? 'https://63ea-13-42-13-51.ngrok-free.app' : 'http://127.0.0.1:5000';
+const apiUrl = process.env.NODE_ENV === 'production' ? 'https://64fe-13-42-13-51.ngrok-free.app' : 'http://127.0.0.1:5000';
 
 export default apiUrl;
