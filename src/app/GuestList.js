23201ad72b1fa4c5ac2ba33db7a8b859b5dbb7e3
Merlin <50379729+AsparAugustus@users.noreply.github.com>
2023-06-21 16:19:36 +0100

    finished

--- src/app/GuestList.js
+++ src/app/GuestList.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-function GuestList({onSwitch}) {
+function GuestList({ onSwitch }) {
   const [guests, setGuests] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
 
@@ -34,57 +34,67 @@ function GuestList({onSwitch}) {
     setSearchQuery(e.target.value);
   }
 
-  const filteredGuests = guests.filter((guest) =>
+  const [filteredGuests, setFilteredGuests] = useState([]);
+
+  useEffect(()=> {
+
+    const _filteredGuests = guests.filter((guest) =>
     guest.full_name.toLowerCase().includes(searchQuery.toLowerCase())
   );
 
+  setFilteredGuests(_filteredGuests)
+  }, [searchQuery])
+
+  
+
   return (
-<div className="flex items-center justify-center h-screen bg-cover bg-center">
-
+    <div className="flex items-center justify-center h-screen bg-cover bg-center">
       <div className="bg-white p-4 rounded-lg shadow-md w-full max-w-3xl">
-        <input
-          type="text"
-          placeholder="Search"
-          value={searchQuery}
-          onChange={handleSearch}
-          className="block w-full py-2 px-3 mb-4 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-200"
-          style={{ minWidth: '200px' }}
-        />
+        <div className="max-h-[80vh] overflow-y-auto">
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={handleSearch}
+            className="block w-full py-2 px-3 mb-4 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:ring focus:ring-blue-200"
+            style={{ minWidth: '200px' }}
+          />
 
-<ul>
-  {!searchQuery ? (
-    <>
-      {/* Code for rendering when searchQuery is false */}
-    </>
-  ) : (
-    filteredGuests.map((guest) => (
-        <li key={guest.email} className="flex items-center justify-between py-2">
-          <div>{guest.full_name}</div>
-          {guest.checked_in === 1 ? (
-            <button className="bg-green-500 border border-black px-4 py-2 rounded-md">
-              &#10003; Checked-In
-            </button>
-          ) : (
-            <button
-              onClick={() => handleCheckIn(guest.email)}
-              className="bg-white border border-black px-4 py-2 rounded-md"
-            >
-              Check-In
-            </button>
-          )}
-        </li>
-      ))
-      
-  )}
-</ul>
+          <ul>
+            {!searchQuery ? (
+              <>
+                {/* Code for rendering when searchQuery is false */}
+              </>
+            ) : (
+              filteredGuests.map((guest) => (
+                <li key={guest.full_name} className="flex items-center justify-between py-2">
+                  <div>{guest.full_name}</div>
+                  {guest.checked_in === 1 ? (
+                    <button className="bg-green-500 border border-black px-4 py-2 rounded-md">
+                      &#10003; Checked-In
+                    </button>
+                  ) : (
+                    <button
+                      onClick={() => handleCheckIn(guest.email)}
+                      className="bg-white border border-black px-4 py-2 rounded-md"
+                    >
+                      Check-In
+                    </button>
+                  )}
+                </li>
+              ))
+            )}
+          </ul>
 
-
-        <button 
-        onClick={() => {onSwitch()}}
-        className="bg-white border border-black px-4 py-2 rounded-md">
-            + Add Guest</button>
-
-
+          <button
+            onClick={() => {
+              onSwitch();
+            }}
+            className="bg-white border border-black px-4 py-2 rounded-md"
+          >
+            + Add Guest
+          </button>
+        </div>
       </div>
     </div>
   );
